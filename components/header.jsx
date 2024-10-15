7beed8965443781018749f0fc0ba9f491c81e444
Mark Lagunez <mark.lagunez@outlook.com>
2024-10-14 21:57:42 -0600

    removing content from base repo

--- components/header.jsx
+++ components/header.jsx
@@ -7,8 +7,6 @@ const navItems = [
     { linkText: 'Home', href: '/' },
     { linkText: 'Revalidation', href: '/revalidation' },
     { linkText: 'Image CDN', href: '/image-cdn' },
-    { linkText: 'Edge Function', href: '/edge' },
-    { linkText: 'Blobs', href: '/blobs' },
     { linkText: 'Classics', href: '/classics' }
 ];
 
